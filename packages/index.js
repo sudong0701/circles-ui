f1fbbad441697a0de874e803f09b419fee47c6d8
sudong0701 <46431309+sudong0701@users.noreply.github.com>
2020-03-02 17:05:41 +0800

    npm包集成rem计算和icon样式

--- packages/index.js
+++ packages/index.js
@@ -19,6 +19,65 @@ if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue)
 }
 
+function flexible (window, document) {
+    var docEl = document.documentElement
+    var dpr = window.devicePixelRatio || 1
+
+    // adjust body font size
+    function setBodyFontSize () {
+        if (document.body) {
+            document.body.style.fontSize = (12 * dpr) + 'px'
+        }
+        else {
+            document.addEventListener('DOMContentLoaded', setBodyFontSize)
+        }
+    }
+    setBodyFontSize();
+
+    // set 1rem = viewWidth / 10
+    function setRemUnit () {
+        var rem = docEl.clientWidth / 7.5
+        docEl.style.fontSize = rem + 'px'
+    }
+
+    setRemUnit()
+
+    // reset rem unit on page resize
+    window.addEventListener('resize', setRemUnit)
+    window.addEventListener('pageshow', function (e) {
+        if (e.persisted) {
+            setRemUnit()
+        }
+    })
+
+    // detect 0.5px supports
+    if (dpr >= 2) {
+        var fakeBody = document.createElement('body')
+        var testElement = document.createElement('div')
+        testElement.style.border = '.5px solid transparent'
+        fakeBody.appendChild(testElement)
+        docEl.appendChild(fakeBody)
+        if (testElement.offsetHeight === 1) {
+            docEl.classList.add('hairlines')
+        }
+        docEl.removeChild(fakeBody)
+    }
+}
+flexible(window, document)
+
+function icon() {
+    const script = document.createElement('script')
+    const css = document.createElement('link')
+    const html = document.getElementsByTagName('html')[0]
+    script.src = '//at.alicdn.com/t/font_1665721_6ut2lmc7vf6.js'
+    script.type = 'text/javascript'
+    css.href = '//at.alicdn.com/t/font_1665721_6ut2lmc7vf6.css'
+    css.rel = 'stylesheet'
+    html.appendChild(css)
+    html.appendChild(script)
+}
+icon()
+
 export default {
     install,
     sdHeader
