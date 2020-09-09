6904c30316f8f42e52e4e9707566fa98b1eeafc7
sudong0701 <46431309+sudong0701@users.noreply.github.com>
2020-09-09 13:54:05 +0800

    Skeleton(骨架屏)组件优化

--- packages/skeleton/src/skeleton.vue
+++ packages/skeleton/src/skeleton.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="csSkeleton-content" :style="avatar ? 'padding-top: 0.16rem' : ''" v-if="isLoad">
             <div  :class="`csSkeleton-title ${animate ? 'csSkeleton-flashing' : ''}`" v-if="title"></div>
-            <div :style="`width: ${computedRowWidth[key] ? computedRowWidth[key] : '100%'}`" :class="`csSkeleton-row ${animate ? 'csSkeleton-flashing' : ''}`" v-for="(item, key) in row" :key="key"></div>
+            <div :style="`width: ${computedRowWidth[key] ? computedRowWidth[key] : '100%'}`" :class="`csSkeleton-row ${animate ? 'csSkeleton-flashing' : ''}`" v-for="(item, key) in Number(row)" :key="key"></div>
         </div>
         <slot v-if="!isLoad"></slot>
     </div>
@@ -24,17 +24,17 @@
             },
             title: {    //是否展示标题占位图
                 type: Boolean,
-                default: true
+                default: false
             },
             avatar: {    //是否展示头像占位图
                 type: Boolean,
-                default: true
+                default: false
             },
             row: {   //配置占位段落行数
                 type: Number | String,
-                default: 3
+                default: 0
             },
-            rowWidth: {
+            rowWidth: {   //配置占位段落宽度
                 type: Array,
                 default: ()=> {
                     return []
@@ -64,7 +64,7 @@
             computedRowWidth() {
                 return this.rowWidth.map((item)=> {
                     if(typeof item === 'number') {
-                        return `${item}px`
+                        return `${item}%`
                     } else if(typeof item === 'string') {
                         return item
                     } else {
@@ -75,54 +75,3 @@
         }
     }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-    .csSkeleton {
-        display: flex;
-        flex-direction: row;
-        .csSkeleton-avatar {
-            margin-right: 0.32rem;
-            >div {
-                background-color: #f2f3f5;
-            }
-        }
-        .csSkeleton-content {
-            flex: 1;
-            .csSkeleton-title {
-                width: 40%;
-                margin-bottom: 0.4rem;
-            }
-            .csSkeleton-row {
-                margin-bottom: 0.24rem;
-            }
-            .csSkeleton-row:nth-last-child(1) {
-                margin-bottom: 0;
-            }
-            >div {
-                height: 0.32rem;
-                background-color: #f2f3f5;
-            }
-        }
-        //旋转动画
-        .csSkeleton-flashing {
-            animation-name: flashing; /*动画的名称 */
-            animation-duration: 1400ms; /*动画从开始到结束的时间*/
-            animation-iteration-count: infinite; /*动画执行多少次的参数*//*以下是兼容ios所需，参数意义与上相同*/
-            -webkit-animation-name: flashing;
-            -webkit-animation-duration: 1400ms;
-            -webkit-animation-iteration-count: infinite;
-        }
-        @keyframes flashing {
-            0% {
-                opacity: 1;
-            }
-            60% {
-                opacity: 0.6;
-            }
-            100% {
-                opacity: 1;
-            }
-        }
-    }
-</style>
